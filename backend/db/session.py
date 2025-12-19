3a3ca08f03ccbfad5e6147223712bea8836dd69d
Joseph Tan <josephtante05@gmail.com>
2025-12-19 13:53:14 +0800

    set pool_pre_ping to true

--- backend/db/session.py
+++ backend/db/session.py
@@ -9,7 +9,7 @@ backend_dir = Path(__file__).resolve().parent.parent
 load_dotenv(backend_dir / ".env")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # have to manually call SessionLocal.commit() to add and send changes to database, commit() auto calls flush()
 Base = declarative_base()
 
